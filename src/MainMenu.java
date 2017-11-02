6819404f894d3452c20bae12ba3aec50913e364a
alex4787 <33261624+alex4787@users.noreply.github.com>
2017-11-02 12:22:55 -0400

    Update MainMenu.java

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,5 +1,20 @@
-public class MainMenu {
+import java.util.*;
+
+public class MainMenu 
 	public static void displayMainMenu() {
-		
+		Scanner sc = new Scanner (System.in);
+	
+		System.out.println("Welcome to this game of Poker!");
+		System.out.println("Please select one of the following options:");
+		System.out.println("1: Play\n2: Rules\n3: Credits\n4: Exit");
+	
+		int userInput = sc.nextInt();
+	
+		do {
+			if (userInput == 1){
+				
+			}
+		}
+	        
 	}
 }
