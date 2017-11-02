5651556fc5e1aaa02ab10bcc8845c7e7a6284600
espidev <aguy867@gmail.com>
2017-11-02 11:45:06 -0400

    Push

--- src/Poker.java
+++ src/Poker.java
@@ -1,8 +1,5 @@
-
 public class Poker {
-
 	public static void main(String[] args) {
 		Card card = new Card(4, Suit.CLOVER);
 	}
-
 }
--- src/Suit.java
+++ src/Suit.java
@@ -1,4 +1,12 @@
 
 public enum Suit {
-	DIAMOND, CLOVER, HEART, SPADE
+	DIAMOND, CLOVER, HEART, SPADE;
+	static boolean compare(Suit suit1, Suit suit2) {
+		if(suit1.ordinal() > suit2.ordinal()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
