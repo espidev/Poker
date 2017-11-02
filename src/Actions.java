92d1d8b867d54fc522009289e73aee0a05a598a9
alex4787 <alex.chan4787@gmail.com>
2017-11-02 12:38:46 -0400

    John

--- src/Actions.java
+++ src/Actions.java
@@ -1,20 +1,33 @@
 public class Actions {
 	public static boolean check(Player player) {
+		int max = 0;
+		for (int i = 0; i < Poker.players.size(); i++) {
+			if (i == 0) {
+				max = Poker.players.get(i).betMoney;
+			} else if (max < Poker.players.get(i).betMoney) {
+				max = Poker.players.get(i).betMoney;
+			}
+		}
 		
+		if (max - player.betMoney < player.money) {
+			return true;	
+		} else {
+			return false;
+		}
 	}
 	public static boolean bet(Player player, int money) {
-		
+		return true;
 	}
 	public static boolean fold(Player player) {
-		
+		return true;
 	}
 	public static boolean call(Player player) {
-		
+		return true;
 	}
 	public static boolean raise(Player player, int money) {
-		
+		return true;
 	}
 	public static boolean allIn(Player player) {
-		
+		return true;
 	}
 }
