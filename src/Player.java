9f653bb560ba639ab11188cbc74ad57069d97bda
espidev <aguy867@gmail.com>
2017-11-02 11:51:17 -0400

    Changes

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,8 @@
+import java.util.List;
 
 public class Player {
-
+	List<Card> cards;
+	int money, betMoney;
+	String name;
+	boolean isAI, stillInRound = true, stillInGame = true, allIn = false;
 }
