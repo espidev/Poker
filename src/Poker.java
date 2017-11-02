cd38397d832e5bf67460e12b9e040cfab8a7b7be
espidev <aguy867@gmail.com>
2017-11-02 12:08:57 -0400

    Changes

--- src/Poker.java
+++ src/Poker.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Poker {
+	public static List<Card> cardsOnStack = new ArrayList<>(), cardsOnTable = new ArrayList<>();
+	public static List<Player> players = new ArrayList<>();
+	
+	public static Player bigBlind, smallBlind;
+	
+	public static int curPlayer, curOrbit;
+	
+	public static boolean inRound = false;
+	
+	public static int getPot() {
+		int sum = 0;
+		for(Player p : players) {
+			
+		}
+	}
+	
 	public static void main(String[] args) {
-		Card card = new Card(4, Suit.CLOVER);
+		while(true) {
+			MainMenu.displayMainMenu();
+		}
 	}
 }
