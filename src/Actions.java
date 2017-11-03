28e5b012354aacbb0d6973e9396efa8d6bfa7a24
Johnnyli5555 <33261583+Johnnyli5555@users.noreply.github.com>
2017-11-03 16:40:21 +0000

    hi
    hi

--- src/Actions.java
+++ src/Actions.java
@@ -1,5 +1,19 @@
 public class Actions {
 	public static boolean check(Player player) {
+		return true;
+	}
+	public static boolean bet(Player player, int money) {
+                player.betMoney += money;
+                player.money -= money;
+                System.out.println("you bet"+money+"amount of money")
+		return true;
+	}
+	public static boolean fold(Player player) {
+		player.stillInRound = false;
+		System.out.println(player.name + " has folded.");
+		return true;
+	}
+	public static boolean call(Player player) {
 		int max = 0;
 		for (int i = 0; i < Poker.players.size(); i++) {
 			if (i == 0) {
@@ -10,20 +24,10 @@ public class Actions {
 		}
 		return true;
 	}
-	public static boolean bet(Player player, int money) {
-		return true;
-	}
-	public static boolean fold(Player player) {
-		player.stillInRound = false;
-		System.out.println(player.name + " has folded.");
-		return true;
-	}
-	public static boolean call(Player player) {
-		return true;
-	}
 	public static boolean raise(Player player, int money) {
-		
-		
+                player.betMoney += money;
+                player.money -= money;
+                System.out.println("you bet"+money+"amount of money")
 		return true;
 	}
 	public static boolean allIn(Player player) {
