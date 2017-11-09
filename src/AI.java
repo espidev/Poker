62f55ce00beba6afc44ad067552787d818ed945c
J2ckL1 <33261733+J2ckL1@users.noreply.github.com>
2017-11-09 17:01:16 +0000

    hi

--- src/AI.java
+++ src/AI.java
@@ -2,16 +2,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /*
- * ADD A TODO HEADER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * POKER PROGRAM
+ * Names: Alex, John, Jack, Devin
+ * Action Class: A basic AI to choose a random option in the actions class
  */
 
 public class AI {
-
-
-	public static int numGenerator (int min, int max) {
-		return (int) (Math.random()*(max-min) + min);
-	}
-
 	public static void calculateTurn(Player player, HashMap<String, BooleanOperation> options) {
 		int rand = (int) (Math.random() * options.size());
 		new ArrayList<>(options.values()).get(rand).run(player);
