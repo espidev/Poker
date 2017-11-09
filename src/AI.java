3a757b799bd31a35cdf06fd53eca4463c1409df2
J2ckL1 <33261733+J2ckL1@users.noreply.github.com>
2017-11-09 16:44:46 +0000

    Ai

--- src/AI.java
+++ src/AI.java
@@ -6,7 +6,26 @@ import java.util.HashMap;
 
 public class AI {
 	
-	public static void calculateTurn(Player player, HashMap<String, BooleanOperation> options) {
+
+public static int numGenerator (int min, int max) {
+	return (int) (Math.random()*(max-min) + min);
+}
+
+public static void calculateTurn(Player player, HashMap<String, BooleanOperation> options) {
+        int probability = numGenerator(0, 99);
 		
+	if (probability < 20) {
+	       Actions.check();
+	} else if (probability => 20 && probability < 40) {
+		Actions.call();
+	} else if (probability => 40 && probability < 60) {
+		Actions.raise();
+	} else if (probability => 60 && probability < 70) {
+		Actions.fold();
+	} else if (probability => 70 && probability < 90) {
+		Actions.bet();
+	} else {
+		Actions.allIn();
 	}
+}
 }
