74e028934a4dd5254c3f681e79a467ee693f688f
alex4787 <alex.chan4787@gmail.com>
2017-11-12 15:52:26 -0500

    ughhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- src/Hands.java
+++ src/Hands.java
@@ -22,118 +22,151 @@ public class Hands {
 	 *  9 = Straight Flush / SF
 	 *  10 = Royal Flush / RF
 	 */
-	
+
 	public static boolean RF (List<Card> c, List<Card> h) {
 		if (c.size() < 5) {
 			return false;
 		}
-		
+
 		sortCards (c, false);
-		
+
 		for (int i = c.size() - 1; i > 3; i--) {
 			if (c.get(i).suit == c.get(i - 4).suit && c.get(i).number == 14 && c.get(i - 4).number == 10) {
 				h = c.subList(i-4, i);
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	public static boolean SF (List<Card> c, List<Card> h) {
 		if (c.size() < 5) {
 			return false;
 		}
 
 		sortCards (c, false);
-		List<Card> temp = null;
-		
+
 		for (int i = c.size() - 1; i > 3; i--) {
-			if (c.get(i).suit == c.get(i - 4).suit) {
-				
+			if (c.get(i).suit == c.get(i - 4).suit && c.get(i).number == c.get(i - 4).number + 4) {
+				h = c.subList(i-4, i);
+				return true;
 			}
 		}
-		
+
+		//Checks if Special Case (A, 2, 3, 4, 5) is present
+		List<Suit> isLow_Suits = null;
+		int num_of_Aces = 0;
+		boolean isLow = false;
+
+		for (int i = 0; i < c.size(); i++) {
+			if (c.get(i).number == 13) {
+				isLow_Suits.add(c.get(i).suit);
+				num_of_Aces++;
+				isLow = true;
+			}
+			if (num_of_Aces >= 4) {
+				break;
+			}
+		}
+
+		if (isLow) {
+			Suit curSuit;
+
+			for (int i = 0; i < isLow_Suits.size(); i++) {
+				curSuit = isLow_Suits.get(i);
+
+				for (int j = c.size() - 1; j > 2; j--) {
+					if (c.get(i).suit == curSuit && c.get(i-3).suit == curSuit && c.get(i).number == 5 && c.get(i).number == 2) {
+						Card ace = new Card (14, curSuit);
+						h.add(ace);
+						h = c.subList(i-3, i);
+						return true;
+					}
+				}	
+			}
+		}
+
 		return false;
 	}
-	
+
 	public static boolean K4 (List<Card> c, List<Card> h) {
 		if (c.size() < 4) {
 			return false;
 		}
-		
+
 		sortCards(c, true);
-		
+
 		for (int i = c.size() - 1; i > 2; i--) {
 			if (c.get(i).number == c.get(i-3).number) {
 				h = c.subList(i-3, i);
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	public static boolean FH (List<Card> c, List<Card> h) {
 		if (c.size() < 5) {
 			return false;
 		}
-		
+
 		return false;
 	}
-	
+
 	public static boolean FL (List<Card> c, List<Card> h) {
 		if (c.size() < 5) {
 			return false;
 		}
-		
+
 		sortCards(c, false);
-		
+
 		for (int i = c.size() - 1; i > 3; i--) {
 			if (c.get(i).suit == c.get(i - 4).suit) {
 				h = c.subList(i-4, i);
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	public static boolean ST (List<Card> c, List<Card> h) {
 		if (c.size() < 5) {
 			return false;
 		}
-		
+
 		return false;
 	}
-	
+
 	public static boolean K3 (List<Card> c, List<Card> h) {
 		if (c.size() < 3) {
 			return false;
 		}
-		
+
 		sortCards(c, true);
-		
+
 		for (int i = c.size() - 1; i > 1; i--) {
 			if (c.get(i).number == c.get(i-2).number) {
 				h = c.subList(i-2, i);
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	public static boolean PP (List<Card> c, List<Card> h) {
 		if (c.size() < 4) {
 			return false;
 		}
-		
+
 		sortCards(c, true);
 		List<Card> firstPair = null;
 		int counter = 0;
 		int j = 0;
-		
+
 		for (int i = c.size() - 1; i > 0; i--) {
 			if (c.get(i).number == c.get(i-1).number) {
 				firstPair.add(c.get(i));
@@ -143,7 +176,7 @@ public class Hands {
 				break;
 			}
 		}
-		
+
 		for (int i = j; i > 0; i--) {
 			if (c.get(i).number == c.get(i-1).number) {
 				h = c.subList(i-1, i);
@@ -151,37 +184,37 @@ public class Hands {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	public static boolean P1 (List<Card> c, List<Card> h) {
 		if (c.size() < 2) {
 			return false;
 		}
-		
+
 		sortCards(c, true);
-		
+
 		for (int i = c.size() - 1; i > 0; i--) {
 			if (c.get(i).number == c.get(i-1).number) {
 				h = c.subList(i-1, i);
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	public static void HC (List<Card> c, List<Card> h) {
 		sortCards(c, true);
 		h.add(c.get(c.size() - 1));
 	}
-	
+
 	public static void sortCards(List<Card> i, boolean m) {
 		//True: Number, then Suit
 		//False: Suit, then Number
 		Card.mode = m;
 		Collections.sort(i);
 	}
-	
+
 }
